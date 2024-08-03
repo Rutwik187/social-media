4522ba812b0f57dd3ce64179a586f859828f1bbb
rutwik187 <rutwikshinde34@gmail.com>
2024-08-03 22:53:38 +0530

    added session and context

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -2,6 +2,7 @@ import { validateRequest } from "@/auth";
 import { redirect } from "next/navigation";
 import { SessionProvider } from "./SessionProvider";
 import Navbar from "./Navbar";
+import MenuBar from "./MenuBar";
 
 const Layout = async ({ children }: { children: React.ReactNode }) => {
   const session = await validateRequest();
@@ -11,7 +12,11 @@ const Layout = async ({ children }: { children: React.ReactNode }) => {
     <SessionProvider value={session}>
       <div className="flex min-h-screen flex-col">
         <Navbar />
-        <div className="mx-auto max-w-7xl p-5">{children}</div>
+        <div className="mx-auto max-w-7xl p-5">
+          <MenuBar className="sticky top-[5.25rem] hidden h-fit flex-none space-y-3 rounded-2xl bg-card px-3 py-5 shadow-sm sm:block lg:px-5 xl:w-80" />
+          {children}
+        </div>
+        <MenuBar className="sticky top-[5.25rem] hidden h-fit flex-none space-y-3 rounded-2xl bg-card px-3 py-5 shadow-sm sm:block lg:px-5 xl:w-80" />
       </div>
     </SessionProvider>
   );
